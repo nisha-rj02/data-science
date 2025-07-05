863bf3bd3daa29961ff30c762b9b739ad21aa10a
nisha-rj02 <np99297063@gmail.com>
2025-07-05 10:42:05 +0530

    data science

---
+++ python/basic.py
@@ -0,0 +1,127 @@
+# print("hello world!")
+# x =5
+# y =6
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x**y)
+# print(x%y)
+# print(8+5-7*6/9)
+
+# a,b = 5,-2
+# c = a%b
+# #d = a//b
+# print(c)
+# #print(d)
+# name = input("name :")
+# print(name)
+# #int input
+# age = int(input("age:"))
+# print(age)
+# print(type(name))
+# print(len(name))
+# lst =[1,2,3,4,5]
+# print(lst)
+# lst.append(500)
+# print(lst)
+# marks = int(input("marks :"))
+# if(marks > 90):
+#     print("A")
+# elif(marks<85|marks>80):
+#     print("B")
+# elif(marks<75|marks>70):
+#     print("C")
+# elif(marks<65|marks>60):
+#     print("D")
+# else:
+#     print("FAIL")
+
+#     info = {
+#         "name" : "nisha"
+#     }
+#     print(info)
+
+# def myself():
+#  print("hello")
+#  myself()
+ 
+
+#  class Student:
+#    def info(self):
+#      name = "nisha"
+#      age = 22
+#      print(name)
+#      print(age)
+#      print("thisisthe info")
+#  st_obj=Student()
+#  st_obj.info()
+
+# class A:
+#     name="ritik"
+
+# class B(A):
+#     classroom = "cse"
+# obj=B()
+# obj.name
+
+# class Grandfather:
+#     pass
+# class Father:
+#     pass
+# class child:
+#     pass
+# obj=child()
+
+# class Father:
+#     pass
+# class Uncle:
+#     pass
+# class Son(Father, Uncle):
+#     pass
+# obj =Son()
+# #ENCAPSULATION
+# # 1 PUBLIC
+# # 2 PRIVATE _   OJBNAME._CLASSNAME__VARIABLENAME
+# # 3 PROTECTED __
+
+# #OVERRIDING
+
+# class Social:
+#     def info(self):
+#         print("Social")
+
+# class B(Social):
+#     def info(self):
+#         print("B")   
+# obj = B()class
+# obj.info()
+#OVERLOADING
+
+# constructor
+
+# class Student:
+#     def__init__(self,name,age):
+
+#     self.name = "name"
+#     self.age = 33
+
+#     def display(self):
+#         print(self.name)
+# obj.name
+# obj.display()
+
+
+
+# TASK SUPERFUNCTION
+#
+class Clg:
+    def __info__(self,name,age):
+      self.name = input("name:")
+      self.age = int(input("age:"))
+    def display(self):
+      
+      class Salary(Clg):
+        def info(self):
+           Private =10,000
+
